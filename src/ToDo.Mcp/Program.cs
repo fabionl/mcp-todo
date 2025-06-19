ad84d006ac3c70a1397b7aa0cf594f7bcb9fcd70
Fabio Lima <>
2025-06-18 17:29:24 -0700

    Refactor MCP server startup to improve logging and application host configuration

--- src/ToDo.Mcp/Program.cs
+++ src/ToDo.Mcp/Program.cs
@@ -7,40 +7,8 @@ using ToDo.Mcp.ToDoItems;
 using ToDo.Mcp.McpEndpoints;
 using ToDo.Mcp.Services.TimeProviders;
 
-var builder = Host.CreateEmptyApplicationBuilder(settings: null);
-
-var homePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-var appDataPath = Path.Combine(homePath, ".todo-mcp");
-if (!Directory.Exists(appDataPath))
-{
-  Directory.CreateDirectory(appDataPath);
-}
-
-var logPath = Path.Combine(appDataPath, "logs");
-
-Log.Logger = new LoggerConfiguration()
-  // .WriteTo.Console()
-  .WriteTo.File(
-    Path.Combine(logPath, "todo-mcp.log"),
-    rollingInterval: RollingInterval.Day
-  )
-  .CreateLogger();
-
-builder.Services.AddLogging(loggingBuilder =>
-{
-  loggingBuilder.AddSerilog();
-});
-
-builder.Services
-  .AddMcpServer()
-  .WithStdioServerTransport()
-  .WithTools<ToDoTools>();
-
-builder.Services
-  .AddSingleton<ITimeProvider, SystemTimeProvider>()
-  .AddSingleton<IToDoService, ToDoService>();
-
-var app = builder.Build();
+var app = AppHost.CreateHostBuilder(args)
+  .Build();
 
 try
 {
@@ -58,3 +26,56 @@ finally
   Log.CloseAndFlush();
 }
 
+public static class AppHost
+{
+  public static IHostBuilder CreateHostBuilder() => CreateHostBuilder([]);
+  public static IHostBuilder CreateHostBuilder(string[] args)
+  {
+    // TODO: check is args include --logs with a location for logs
+    var homePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+    var appDataPath = Path.Combine(homePath, ".todo-mcp");
+    if (!Directory.Exists(appDataPath))
+    {
+      Directory.CreateDirectory(appDataPath);
+    }
+
+    var logPath = Path.Combine(appDataPath, "logs");
+
+    Log.Logger = new LoggerConfiguration()
+      .WriteTo.Console(standardErrorFromLevel: Serilog.Events.LogEventLevel.Verbose)
+      .WriteTo.File(
+        Path.Combine(logPath, "todo-mcp.log"),
+        rollingInterval: RollingInterval.Day
+      )
+      .CreateLogger();
+
+    var builder = Host.CreateDefaultBuilder(args)
+      .ConfigureServices((hostContext, services) =>
+      {
+        services.AddAppServices();
+      });
+
+
+
+    return builder;
+  }
+
+  private static IServiceCollection AddAppServices(this IServiceCollection services)
+  {
+    services.AddLogging(loggingBuilder =>
+    {
+      loggingBuilder.AddSerilog();
+    });
+
+    services
+      .AddMcpServer()
+      .WithStdioServerTransport()
+      .WithTools<ToDoTools>();
+
+    services
+      .AddSingleton<ITimeProvider, SystemTimeProvider>()
+      .AddSingleton<IToDoService, ToDoService>();
+
+    return services;
+  }
+}
